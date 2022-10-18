21afc9432da32a9167d1570d4e09c282f7750038
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-10-18 12:19:46 +0700

    phuong thuc edit update

--- customer-management-thymleaf/src/main/java/com/codegym/controller/CustomerController.java
+++ customer-management-thymleaf/src/main/java/com/codegym/controller/CustomerController.java
@@ -6,6 +6,7 @@ import com.codegym.service.ICustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -36,6 +37,19 @@ public class CustomerController {
         customer.setId((int) (Math.random() * 10000));
         customerService.save(customer);
         redirect.addFlashAttribute("success", "Created customer successfully!");
+        return "redirect:/customer";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Customer customer, RedirectAttributes redirect) {
+        customerService.update(customer.getId(), customer);
+        redirect.addFlashAttribute("success", "Edited customer successfully!");
         return "redirect:/customer";
     }
 }
