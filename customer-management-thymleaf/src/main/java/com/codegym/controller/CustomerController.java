9d1a495a125e86af1060770aa683ca30e1ab3d75
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-10-18 11:50:58 +0700

    tao du an + phuong thuc create

---
+++ customer-management-thymleaf/src/main/java/com/codegym/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.codegym.controller;
+
+import com.codegym.model.Customer;
+import com.codegym.service.CustomerService;
+import com.codegym.service.ICustomerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    private final ICustomerService customerService = new CustomerService();
+
+    @GetMapping("")
+    public String index(Model model) {
+
+        List<Customer> customerList = customerService.findAll();
+        model.addAttribute("customers", customerList);
+        return "/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Customer customer, RedirectAttributes redirect) {
+        customer.setId((int) (Math.random() * 10000));
+        customerService.save(customer);
+        redirect.addFlashAttribute("success", "Created customer successfully!");
+        return "redirect:/customer";
+    }
+}
